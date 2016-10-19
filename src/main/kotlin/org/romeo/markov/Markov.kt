759ea5dc16265ecf7398da49885741cdf3af7418
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-10-18 20:39:54 -0400

    hello world initial commit

---
+++ src/main/kotlin/org/romeo/markov/Markov.kt
@@ -0,0 +1,14 @@
+package org.romeo.markov
+
+/**
+ * User: tylerromeo
+ * Date: 10/18/16
+ * Time: 8:26 PM
+ *
+ */
+class Markov {
+}
+
+fun main(args: Array<String>) {
+    print("Hello world")
+}
